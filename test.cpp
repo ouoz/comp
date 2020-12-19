532563b0e6e033da0dd493ac537ba6a78d01797b
ouoz <ouoz1V@gmail.com>
2020-12-19 21:30:27 +0900

    init

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(a) (a).begin(),(a).end()
+#define rep(i,n) for(int (i)=0; (i)<(n); (i)++)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define eb emplace_back
+#define fi first
+#define se second
+#define wt(a) cout<<a[0];for(int i=1; i<a.size(); i++)cout<<" "<<a[i];cout<<endl;
+#define V vector
+
+template<class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+
+template<class T> vector<T> vec(int n, T val){vector<T>ret(n, val);return ret;}
+
+using ll=long long;
+
+
+
+signed main(){
+    int a,b,x,y;
+    cin>>a>>b>>x>>y;
+
+    if (2*x<y){
+        
+    }    
+}
